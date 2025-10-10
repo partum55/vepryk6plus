d32aa66ad96e715abb739f8db193112b97b7da85
Ivan Maksymchuk <ivan.maksymchuk4@gmail.com>
2025-10-10 16:57:16 +0300

    trying to send some messages except heartbeat

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <termios.h>
 #include <errno.h>
 #include <time.h>
+#include <sys/select.h>
 
 #include "common/mavlink.h"
 
@@ -14,31 +15,10 @@
 #define SYSTEM_ID 255
 #define COMPONENT_ID 1
 
-void handle_mavlink_message(mavlink_message_t *msg) {
-    switch (msg->msgid) {
-        case MAVLINK_MSG_ID_HEARTBEAT: {
-            mavlink_heartbeat_t heartbeat;
-            mavlink_msg_heartbeat_decode(msg, &heartbeat);
-            printf("Received HEARTBEAT from system %d, component %d. Vehicle is in state %d.\n",
-                   msg->sysid, msg->compid, heartbeat.system_status);
-            break;
-        }
-        case MAVLINK_MSG_ID_STATUSTEXT: {
-            mavlink_statustext_t status;
-            mavlink_msg_statustext_decode(msg, &status);
-            printf("Received STATUSTEXT: %s\n", status.text);
-            break;
-        }
-        case MAVLINK_MSG_ID_COMMAND_ACK: {
-            mavlink_command_ack_t ack;
-            mavlink_msg_command_ack_decode(msg, &ack);
-            printf("Received COMMAND_ACK: Command %d, Result %d\n", ack.command, ack.result);
-            break;
-        }
-        default:
-            break;
-    }
-}
+void handle_mavlink_message(mavlink_message_t *msg);
+void handle_user_command(const char *command, int serial_fd);
+void send_mavlink_arm_command(int serial_fd, int arming_state);
+
 
 int main() {
     int serial_fd;
@@ -50,11 +30,7 @@ int main() {
     }
 
     struct termios tty;
-    if (tcgetattr(serial_fd, &tty) != 0) {
-        perror("Error from tcgetattr");
-        close(serial_fd);
-        return 1;
-    }
+    tcgetattr(serial_fd, &tty);
     cfsetospeed(&tty, BAUD_RATE);
     cfsetispeed(&tty, BAUD_RATE);
     tty.c_cflag &= ~PARENB;
@@ -64,16 +40,11 @@ int main() {
     tty.c_cflag |= CREAD | CLOCAL;
     tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
     tty.c_oflag &= ~OPOST;
-    // Set VMIN = 0, VTIME = 0 for non-blocking read
     tty.c_cc[VMIN] = 0;
     tty.c_cc[VTIME] = 0;
-    if (tcsetattr(serial_fd, TCSANOW, &tty) != 0) {
-        perror("Error from tcsetattr");
-        close(serial_fd);
-        return 1;
-    }
+    tcsetattr(serial_fd, TCSANOW, &tty);
 
-    printf("Serial port configured. Starting MAVLink communication...\n");
+    printf("Serial port configured. Type commands like 'arm' or 'disarm' and press Enter.\n");
 
     time_t last_heartbeat_time = 0;
 
@@ -81,47 +52,120 @@ int main() {
         time_t current_time = time(NULL);
         if (current_time - last_heartbeat_time >= 1) {
             last_heartbeat_time = current_time;
-
             mavlink_message_t msg;
             uint8_t buf[MAVLINK_MAX_PACKET_LEN];
-
-            mavlink_msg_heartbeat_pack(
-                SYSTEM_ID,
-                COMPONENT_ID,
-                &msg,
-                MAV_TYPE_GCS,
-                MAV_AUTOPILOT_GENERIC,
-                MAV_MODE_FLAG_MANUAL_INPUT_ENABLED,
-                0,
-                MAV_STATE_ACTIVE
-            );
-
+            mavlink_msg_heartbeat_pack(SYSTEM_ID, COMPONENT_ID, &msg, MAV_TYPE_GCS, MAV_AUTOPILOT_GENERIC, MAV_MODE_FLAG_MANUAL_INPUT_ENABLED, 0, MAV_STATE_ACTIVE);
             uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-
-            ssize_t bytes_written = write(serial_fd, buf, len);
-            if (bytes_written > 0) {
-                printf("Heartbeat sent (%zd bytes).\n", bytes_written); // NOISE
-            } else {
-                perror("Failed to write to serial port");
-            }
+            write(serial_fd, buf, len);
         }
 
-        uint8_t read_buf[MAVLINK_MAX_PACKET_LEN];
-        mavlink_message_t received_msg;
-        mavlink_status_t status;
+        // SETUP I/O MULTIPLEXING WITH select()
+        fd_set read_fds;
+        FD_ZERO(&read_fds);
+        FD_SET(serial_fd, &read_fds);         // Monitor the serial port
+        FD_SET(STDIN_FILENO, &read_fds);      // Monitor the keyboard (stdin)
 
-        ssize_t bytes_read = read(serial_fd, read_buf, sizeof(read_buf));
-        if (bytes_read > 0) {
-            for (int i = 0; i < bytes_read; ++i) {
-                if (mavlink_parse_char(MAVLINK_COMM_0, read_buf[i], &received_msg, &status)) {
-                    handle_mavlink_message(&received_msg);
+        struct timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = 50000; // 50ms timeout
+
+        int max_fd = (serial_fd > STDIN_FILENO) ? serial_fd : STDIN_FILENO;
+
+        // select() blocks until data is available or the timeout occurs
+        int activity = select(max_fd + 1, &read_fds, NULL, NULL, &timeout);
+
+        if (activity < 0) {
+            perror("select error");
+            break;
+        }
+
+        if (activity > 0) {
+            // Check for incoming MAVLink messages
+            if (FD_ISSET(serial_fd, &read_fds)) {
+                uint8_t read_buf[MAVLINK_MAX_PACKET_LEN];
+                mavlink_message_t received_msg;
+                mavlink_status_t status;
+                ssize_t bytes_read = read(serial_fd, read_buf, sizeof(read_buf));
+                if (bytes_read > 0) {
+                    for (int i = 0; i < bytes_read; ++i) {
+                        if (mavlink_parse_char(MAVLINK_COMM_0, read_buf[i], &received_msg, &status)) {
+                            handle_mavlink_message(&received_msg);
+                        }
+                    }
+                }
+            }
+
+            // Check for user keyboard input
+            if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+                char user_buf[100];
+                if (fgets(user_buf, sizeof(user_buf), stdin)) {
+                    user_buf[strcspn(user_buf, "\n")] = 0;
+                    handle_user_command(user_buf, serial_fd);
                 }
             }
         }
-
-        usleep(10000); // 1ms sleep
     }
 
     close(serial_fd);
     return 0;
+}
+
+void handle_user_command(const char *command, int serial_fd) {
+    if (strcmp(command, "arm") == 0) {
+        printf("Sending ARM command...\n");
+        send_mavlink_arm_command(serial_fd, 1); // 1 to arm
+    } else if (strcmp(command, "disarm") == 0) {
+        printf("Sending DISARM command...\n");
+        send_mavlink_arm_command(serial_fd, 0); // 0 to disarm
+    } else {
+        printf("Unknown command: '%s'\n", command);
+    }
+}
+
+// Function to send an arm/disarm command
+void send_mavlink_arm_command(int serial_fd, int arming_state) {
+    mavlink_message_t msg;
+    uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+
+    // MAV_CMD_COMPONENT_ARM_DISARM
+    mavlink_msg_command_long_pack(
+        SYSTEM_ID,                // This system's ID
+        COMPONENT_ID,             // This system's component ID
+        &msg,
+        1,                        // Target System ID (1 is typically the vehicle)
+        0,                        // Target Component ID (0 means broadcast to all components)
+        MAV_CMD_COMPONENT_ARM_DISARM,
+        0,                        // Confirmation
+        (float)arming_state,      // param1: 1 to arm, 0 to disarm
+        0.0f,                     // param2: Not used
+        0.0f,                     // param3: Not used
+        0.0f,                     // param4: Not used
+        0.0f,                     // param5: Not used
+        0.0f,                     // param6: Not used
+        0.0f                      // param7: Not used
+    );
+
+    uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+    write(serial_fd, buf, len);
+}
+
+// Function to handle incoming MAVLink messages
+void handle_mavlink_message(mavlink_message_t *msg) {
+    switch (msg->msgid) {
+        case MAVLINK_MSG_ID_HEARTBEAT:
+            printf("Received HEARTBEAT from vehicle.\n");
+            break;
+        case MAVLINK_MSG_ID_STATUSTEXT:
+            mavlink_statustext_t status;
+            mavlink_msg_statustext_decode(msg, &status);
+            printf("VEHICLE STATUS: %s\n", status.text);
+            break;
+        case MAVLINK_MSG_ID_COMMAND_ACK:
+            mavlink_command_ack_t ack;
+            mavlink_msg_command_ack_decode(msg, &ack);
+            printf("Received ACK for command %d with result %d\n", ack.command, ack.result);
+            break;
+        default:
+            break;
+    }
 }
